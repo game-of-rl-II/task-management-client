54637f4635ef72f46241d5c0d81493cbd40f82d4
Arif6474 <arifulislam64743@gmail.com>
2022-08-14 00:22:43 +0600

    update design of manage employee page

--- src/Components/Dashboard/AdminDashboardItems/ManageEmployee.js
+++ src/Components/Dashboard/AdminDashboardItems/ManageEmployee.js
@@ -1,80 +1,95 @@
 // import React, { useEffect, useState } from 'react';
 
 const ManageEmployee = () => {
-    const employees = [
-        {
-          "_id": "546",
-          "name": "Haider Nayeem"
-        },
-        {
-          "_id": "462",
-          "name": "Mahmudul Hasan"
-        },
-        {
-          "_id": "646",
-          "name": "Jobair Sarkar"
-        },
-        {
-          "_id": "985",
-          "name": "Shahid Islam"
-        },
-        {
-          "_id": "975",
-          "name": "Saif Emon"
-        }
-      ]
-    // const [employees, setEmployees] = useState([]);
-    // useEffect(() => {
-    //     fetch('employee.json')
-    //     .then(res => res.json())
-    //     .then(data => setEmployees(data))
-    // }, [])
-    return (
-        <div >
-            <h1 className=" bg-slate-900 w-60 mx-auto py-1 rounded  text-center text-white my-8 font-bold">MANAGE ALL EMPLOYEE</h1>
-            <div className="w-full ">
-  <table className="table w-3/4 mx-auto ">
+  const employees = [
+    {
+      _id: "546",
+      name: "Haider Nayeem",
+      image: "https://placeimg.com/192/192/people",
+    },
+    {
+      _id: "462",
+      name: "Mahmudul Hasan",
+      image: "https://placeimg.com/192/192/people",
+    },
+    {
+      _id: "646",
+      name: "Jobair Sarkar",
+      image: "https://placeimg.com/192/192/people",
+    },
+    {
+      _id: "985",
+      name: "Shahid Islam",
+      image: "https://placeimg.com/192/192/people",
+    },
+    {
+      _id: "975",
+      name: "Saif Emon",
+      image: "https://placeimg.com/192/192/people",
+    },
+  ];
+  // const [employees, setEmployees] = useState([]);
+  // useEffect(() => {
+  //     fetch('employee.json')
+  //     .then(res => res.json())
+  //     .then(data => setEmployees(data))
+  // }, [])
+  return (
+    <div>
+      <h1 className=" bg-slate-900 w-60 mx-auto py-1 rounded  text-center text-white my-8 font-bold">
+        MANAGE ALL EMPLOYEE
+      </h1>
+      <div className="w-full ">
+        <table className="table w-3/4 mx-auto ">
+          <thead>
+            <tr>
+              <th></th>
+              <th className="text-sm font-bold">EMPLOYEE NAME</th>
+              <th className="text-sm font-bold">EMPLOYEE ID</th>
+              <th className="text-sm font-bold">NEW TASK</th>
+              <th className="text-sm font-bold">DELETE EMPLOYEE</th>
+            </tr>
+          </thead>
+          <tbody>
+            {employees.map((employee , index) => (
+              <tr key={employee._id}>
+                <th>
+                  {index + 1}
 
-    <thead>
-      <tr>
-        <th></th>
-        <th className="text-sm font-bold">EMPLOYEE NAME</th>
-        <th className="text-sm font-bold">EMPLOYEE ID</th>
-        <th className="text-sm font-bold">ASSIGN TASK</th>
-        <th className="text-sm font-bold">DELETE EMPLOYEE</th>
+                </th>
+                <td>
+                  <div class="flex items-center space-x-3">
+                    <div class="avatar">
+                      <div class="w-14 mr-4 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                        <img src={employee.image} alt="" />
+                      </div>
+                    </div>
+                    <div>
+                      <div class="font-bold">{employee.name}</div>
+                      
+                    </div>
+                  </div>
+                </td>
 
-      </tr>
-    </thead>
-    <tbody>
+                <td className="text-xs font-bold">{employee._id}</td>
 
-      {
-        employees.map(employee => <tr key={employee._id}>
-            <th>
-              <label>
-                <input type="checkbox" className="checkbox checkbox-accent" />
-              </label>
-            </th>
-            <td className=" ">
-              {employee.name}
-            </td>
-            <td className="text-xs font-bold">
-             {employee._id}
-            </td>
-
-            <th>
-              <button className="btn btn-outline btn-success  btn-xs">Assign</button>
-            </th>
-            <th>
-              <button className="btn btn-outline btn-error  btn-xs">Delete</button>
-            </th>
-          </tr>)
-      }
-
-    </tbody>
-  </table>
-</div>
-        </div>
-    );
+                <th>
+                  <button className="btn btn-outline btn-success btn-sm">
+                    Assign
+                  </button>
+                </th>
+                <th>
+                  <button className="btn btn-outline btn-error  btn-sm ">
+                    Delete
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default ManageEmployee;
