4203687ac3926e6ad48501261410d8a3fee3a41d
al-amin <arifurr231@gmail.com>
2022-08-14 15:16:01 +0600

    faq & about

---
+++ src/Components/Home/FAQ.js
@@ -0,0 +1,75 @@
+import React from "react";
+
+const FAQ = () => {
+  return (
+    <>
+      <div>
+        <section class="text-gray-700">
+          <div class="container px-5 py-24 mx-auto">
+            <div class="text-center mb-20">
+              <h1 class="sm:text-3xl text-2xl font-medium text-center title-font text-gray-900 mb-4">Frequently Asked Question</h1>
+              <p class="text-base leading-relaxed xl:w-2/4 lg:w-3/4 mx-auto">
+                The most common questions about how our Projects works and what services we provide.
+              </p>
+            </div>
+            <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+              <div class="w-full lg:w-1/2 px-4 py-2">
+                <details class="mb-4">
+                  <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">What is Projects Management ?</summary>
+
+                  <span>
+                    Project management is the process of leading the work of a team to achieve all project goals within the given constraints.
+                  </span>
+                </details>
+                <details class="mb-4">
+                  <summary class="font-semibold bg-gray-200 rounded-md py-2 px-4">Tell About Your Project Management.</summary>
+
+                  <span>
+                    Laboris qui labore cillum culpa in sunt quis sint veniam. Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis minim velit
+                    nostrud pariatur culpa magna in aute.
+                  </span>
+                </details>
+                <details class="mb-4">
+                  <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">About your Employees?</summary>
+
+                  <span>
+                    Laboris qui labore cillum culpa in sunt quis sint veniam. Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis minim velit
+                    nostrud pariatur culpa magna in aute.
+                  </span>
+                </details>
+              </div>
+              <div class="w-full lg:w-1/2 px-4 py-2">
+                <details class="mb-4">
+                  <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">About your Employees?</summary>
+
+                  <span class="px-4 py-2">
+                    Laboris qui labore cillum culpa in sunt quis sint veniam. Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis minim velit
+                    nostrud pariatur culpa magna in aute.
+                  </span>
+                </details>
+                <details class="mb-4">
+                  <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">How many sites I can create at once?</summary>
+
+                  <span class="px-4 py-2">
+                    Laboris qui labore cillum culpa in sunt quis sint veniam. Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis minim velit
+                    nostrud pariatur culpa magna in aute.
+                  </span>
+                </details>
+                <details class="mb-4">
+                  <summary class="font-semibold  bg-gray-200 rounded-md py-2 px-4">How can I communicate with you?</summary>
+
+                  <span class="px-4 py-2">
+                    Laboris qui labore cillum culpa in sunt quis sint veniam. Dolore ex aute deserunt esse ipsum elit aliqua. Aute quis minim velit
+                    nostrud pariatur culpa magna in aute.
+                  </span>
+                </details>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default FAQ;
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,7 +1,16 @@
 import React from "react";
+import About from "./About";
+import FAQ from "./FAQ";
+import HomeBanner from "./HomeBanner";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="">
+      <HomeBanner />
+      <About />
+      <FAQ />
+    </div>
+  );
 };
 
 export default Home;
