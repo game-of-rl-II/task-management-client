5ceac1becc1314f9d9430f45e7851f16f30495b6
faridulhaque <faridulhaquemurshed@gmail.com>
2022-08-14 17:12:38 +0600

    nabin merged

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 import React from "react";
 import About from "./About";
 import FAQ from "./FAQ";
 import HomeBanner from "./HomeBanner";
 
-=======
-import React from 'react';
-import Dashboard from '../Dashboard/Dashboard';
->>>>>>> 9e6dd6532a66d77009638e3c9d88aa8ed7403f09
 const Home = () => {
   return (
     <div className="">
