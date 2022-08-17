b4f226230a192c300401930d59dfd320d54c65d0
al-amin <arifurr231@gmail.com>
2022-08-17 13:18:57 +0600

    complete page dynamic

--- src/Components/Home/HomeBanner.js
+++ src/Components/Home/HomeBanner.js
@@ -8,25 +8,30 @@ const HomeBanner = () => {
     <div>
       <div class="carousel w-full h-screen max-h-[80vh]">
         <div id="item1" class="carousel-item w-full">
-          {/* <div
+          <div
             style={{
               background: `url(${Banner3})`,
             }}
-            className="w-full
-      bg-no-repeat
-      bg-cover
-      "
-          ></div> */}
-          <img src={Banner3} class="w-full" />
+            className="w-full bg-no-repeat bg-cover"
+          >
+            {" "}
+            <div className="flex flex-col items-center justify-center h-full  ">
+              <h1 className="lg:leading-10  text-sm md:text-xl font-bold w-3/6  text-center">
+                Project management is the process of leading the work of a team to achieve all project goals within the given constraints.
+              </h1>
+              <button className="btn btn-primary">Get start</button>
+            </div>
+          </div>
+          {/* <img src={Banner3} class="w-full" alt="" /> */}
         </div>
         <div id="item2" class="carousel-item w-full">
-          <img src={Banner2} class="w-full" />
+          <img src={Banner2} class="w-full" alt="" />
         </div>
         <div id="item3" class="carousel-item w-full">
           <img src={Banner4} class="w-full " alt="" />
         </div>
         <div id="item4" class="carousel-item w-full">
-          <img src="https://placeimg.com/800/200/arch" class="w-full" />
+          <img src="https://placeimg.com/800/200/arch" class="w-full" alt="" />
           <a href="#item4" class="btn btn-xs btn-primary ">
             4
           </a>
