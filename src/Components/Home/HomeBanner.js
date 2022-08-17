4bbf0a12bdc52d5080132d45870f14bd6d49047d
al-amin <arifurr231@gmail.com>
2022-08-18 01:41:30 +0600

    navbar update

--- src/Components/Home/HomeBanner.js
+++ src/Components/Home/HomeBanner.js
@@ -1,7 +1,28 @@
 import React from "react";
+import Banner from "../../Images/banner-2.2.jpg";
 
 const HomeBanner = () => {
-  return <div>home</div>;
+  return (
+    <div className="w-full absolute top-0">
+      <div
+        class="hero w-ful min-h-screen bg-no-repeat
+      bg-right-bottom"
+        style={{ background: `url(${Banner}` }}
+      >
+        <div class="hero-overlay bg-opacity-60"></div>
+        <div class="hero-content text-center text-neutral-content">
+          <div class="max-w-md mt-28">
+            <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+            <p class="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+              a id nisi.
+            </p>
+            <button class="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomeBanner;
--- src/Components/Shared/Navbar/Navbar.js
+++ src/Components/Shared/Navbar/Navbar.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="navbar bg-base-100">
+    <div className="">
+      <div className="navbar relative z-50 top-5">
         <div className="flex-1">
-          <Link to="/" className="text-3xl">TM Tool</Link>
+          <Link to="/" className="text-3xl text-primary">
+            TM Tool
+          </Link>
         </div>
         <div className="flex-none">
           <ul className="menu menu-horizontal p-0">
-            <li><Link className="nav-button mr-3 btn btn-primary" to="/adminLogin">Admin Login</Link></li>
+            <li>
+              <Link className="nav-button mr-3 btn btn-primary" to="/adminLogin">
+                Admin Login
+              </Link>
+            </li>
 
-            <li><Link className="nav-button mr-3 btn btn-primary" to="/employeeLogin">Employee Login</Link></li>
+            <li>
+              <Link className="nav-button mr-3 btn btn-primary" to="/employeeLogin">
+                Employee Login
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
