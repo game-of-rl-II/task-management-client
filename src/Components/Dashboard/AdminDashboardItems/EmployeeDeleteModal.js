62ced3ecd242543310192ee24e78c7500b83c80d
ArifulIslamShuvo <arifulislams063@gmail.com>
2022-08-18 02:07:01 +0600

    update modal

--- src/Components/Dashboard/AdminDashboardItems/EmployeeDeleteModal.js
+++ src/Components/Dashboard/AdminDashboardItems/EmployeeDeleteModal.js
@@ -5,11 +5,12 @@ const EmployeeDeleteModal = () => {
         <div>
             <input type="checkbox" id="EmployeeDelete-modal" class="modal-toggle" />
             <div class="modal modal-bottom sm:modal-middle">
-                <div class="modal-box">
-                    <h3 class="font-bold text-lg">Congratulations random Internet user!</h3>
-                    <p class="py-4">Are you sure? You want to Delete?</p>
+                <div class="modal-box bg-gradient-to-t from-pink-500 to-orange-400 border-y-4 border-white">
+                    <h3 class="font-bold text-lg">Are you sure? You want to Delete?</h3>
+                    <p class="py-4"></p>
                     <div class="modal-action">
-                        <label for="EmployeeDelete-modal" class="btn">Yay!</label>
+                        <label for="EmployeeDelete-modal" class="btn btn-success btn-sm px-5">CANCEL</label>
+                        <label for="EmployeeDelete-modal" class="btn bg-white border-0 btn-sm text-red-600 px-5">Delete</label>
                     </div>
                 </div>
             </div>
