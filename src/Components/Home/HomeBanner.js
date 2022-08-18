3d3933612acac58c015b04ba24aa937aa91d94f6
al-amin <arifurr231@gmail.com>
2022-08-18 14:27:11 +0600

    congfig file change

--- src/Components/Home/HomeBanner.js
+++ src/Components/Home/HomeBanner.js
@@ -26,12 +26,3 @@ const HomeBanner = () => {
 };
 
 export default HomeBanner;
-
-// <div className="h-full bg-black-rgba">
-//   <div className="flex flex-col items-center justify-center h-full  ">
-//     <h1 className="lg:leading-10  text-sm md:text-xl font-bold w-3/6 text-white text-center">
-//       Project management is the process of leading the work of a team to achieve all project goals within the given constraints.
-//     </h1>
-//     <button className="btn btn-primary btn-lg mt-8">Get start</button>
-//   </div>
-// </div>;
