3255f8214a6067a8bf92ae4c66679427d781eefd
Mohammed Jewel <jewelnabin@gmail.com>
2022-08-19 23:24:20 +0600

    neew navbar

--- src/Components/Home/HomeBanner.js
+++ src/Components/Home/HomeBanner.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../Firebase/firebase.init";
-import Banner from "../../Images/banner-2.2.jpg";
+// import Banner from "../../Images/task-tool.png";
 
 const HomeBanner = () => {
   const navigate = useNavigate()
@@ -14,24 +14,9 @@ const HomeBanner = () => {
   }
   return (
     <div className="w-full absolute top-0">
-      <div
-        className="hero w-ful min-h-screen bg-no-repeat
-      bg-right-bottom"
-        style={{ background: `url(${Banner}` }}
-      >
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md mt-28">
-            <h1 className="mb-5 text-4xl font-bold">Welcome to TM Tool</h1>
-            <p className="mb-5 text-2xl">
-              Manage your team from anywhere!
-            </p>
-            {
-              (!admin?.uid && !member) &&
-              <button onClick={() => navigate('/register')} className="btn btn-primary">Get Started</button>
-            }
-          </div>
-        </div>
+      <div>
+        {/* <div>left</div>
+        <div>rigt</div> */}
       </div>
     </div>
   );
