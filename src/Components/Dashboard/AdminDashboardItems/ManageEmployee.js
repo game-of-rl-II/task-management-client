8a7ab860a967aaa2c971489d6e151af4564e8e2d
faridulhaque <faridulhaquemurshed@gmail.com>
2022-08-23 11:32:38 +0600

    changes

--- src/Components/Dashboard/AdminDashboardItems/ManageEmployee.js
+++ src/Components/Dashboard/AdminDashboardItems/ManageEmployee.js
@@ -14,7 +14,7 @@ const ManageEmployee = () => {
   const [deleteMember, setDeleteMember] = useState(null);
   const [assignTaskMember, setAssignTaskMember] = useState(null);
   const { teamName } = useTeamName()
-  
+
 
   const {
     data: members,
@@ -31,58 +31,58 @@ const ManageEmployee = () => {
     return <Loading />
 
   }
-    return (
-      <div>
-        <h1 className=" bg-secondary w-52 mx-auto py-1 rounded  text-center text-white my-8 font-bold">MANAGE ALL MEMBER</h1>
-        <div className="w-full ">
-          <table className="table w-3/4 mx-auto ">
-            <thead>
-              <tr>
-                <th></th>
-                <th className="text-sm font-bold">MEMBER NAME</th>
-                <th className="text-sm font-bold">MEMBER ID</th>
-                <th className="text-sm font-bold">NEW TASK</th>
-                <th className="text-sm font-bold">DELETE MEMBER</th>
-              </tr>
-            </thead>
-            <tbody>
-              {members.map((member, index) => (
-                <tr key={member._id}>
-                  <th>{index + 1}</th>
-                  <td>
-                    <div className="flex items-center space-x-3">
-                      <div className="avatar">
-                        <div className="w-14 mr-4 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-                          <img src={member.image} alt="" />
-                        </div>
-                      </div>
-                      <div>
-                        <div className="font-bold">{member.name}</div>
+  return (
+    <div>
+      <h1 className=" bg-secondary w-52 mx-auto py-1 rounded  text-center text-white my-8 font-bold">MANAGE ALL MEMBER</h1>
+      <div className="w-full ">
+        <table className="table w-3/4 mx-auto ">
+          <thead>
+            <tr>
+              <th></th>
+              <th className="text-sm font-bold">MEMBER NAME</th>
+              <th className="text-sm font-bold">MEMBER ID</th>
+              <th className="text-sm font-bold">NEW TASK</th>
+              <th className="text-sm font-bold">DELETE MEMBER</th>
+            </tr>
+          </thead>
+          <tbody>
+            {members.map((member, index) => (
+              <tr key={member._id}>
+                <th>{index + 1}</th>
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="w-14 mr-4 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                        <img src={member.image} alt="" />
                       </div>
                     </div>
-                  </td>
+                    <div>
+                      <div className="font-bold">{member.name}</div>
+                    </div>
+                  </div>
+                </td>
 
-                  <td className="text-xs font-bold">{member.id}</td>
+                <td className="text-xs font-bold">{member.id}</td>
 
-                  <th>
-                    <label onClick={() => setAssignTaskMember(member)} htmlFor="my-modal-6" className="btn modal-button btn-outline btn-success btn-sm">
-                      Assign
-                    </label>
-                    {assignTaskMember && <AssignTaskModal assignTaskMember={assignTaskMember} setAssignTaskMember={setAssignTaskMember} />}
-                  </th>
-                  <th>
-                    <label onClick={() => setDeleteMember(member)} htmlFor="EmployeeDelete-modal" className="btn modal-button btn-outline btn-error  btn-sm">
-                      Delete
-                    </label>
-                    {deleteMember && <EmployeeDeleteModal deleteMember={deleteMember} />}
-                  </th>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+                <th>
+                  <label onClick={() => setAssignTaskMember(member)} htmlFor="my-modal-6" className="btn modal-button btn-outline btn-success btn-sm">
+                    Assign
+                  </label>
+                  {assignTaskMember && <AssignTaskModal assignTaskMember={assignTaskMember} setAssignTaskMember={setAssignTaskMember} />}
+                </th>
+                <th>
+                  <label onClick={() => setDeleteMember(member)} htmlFor="EmployeeDelete-modal" className="btn modal-button btn-outline btn-error  btn-sm">
+                    Delete
+                  </label>
+                  {deleteMember && <EmployeeDeleteModal deleteMember={deleteMember} />}
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
-    );
-  };
+    </div>
+  );
+};
 
-  export default ManageEmployee;
+export default ManageEmployee;
