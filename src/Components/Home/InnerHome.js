b65848ac015aef38a64c411f5462ca8ad706b353
faridulhaque <faridulhaquemurshed@gmail.com>
2022-08-24 17:42:51 +0600

    updated

--- src/Components/Home/InnerHome.js
+++ src/Components/Home/InnerHome.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link, Outlet } from 'react-router-dom';
 import { auth } from '../../Firebase/firebase.init';
@@ -8,22 +8,27 @@ import './InnerHome.css'
 
 const InnerHome = () => {
   const [admin, adminLoading, adminError] = useAuthState(auth);
+  const [teams, setTeams] = useState([]);
   const email = admin?.email;
 
-  const {
-    data: teams,
-    isLoading,
+  useEffect(() => {
+    fetch(`http://localhost:5000/teams/${email}`)
+    .then(res => res.json())
+    .then(data => setTeams(data))
+  },[email, teams])
 
-  } = useQuery(["teamOwner", email], () =>
-    fetch(`http://localhost:5000/teams/${email}`, {
-      method: "GET",
-    }).then((res) => res.json())
-  );
+  // const {
+  //   data: teams,
+  //   isLoading,
+
+  // } = useQuery(["teamOwner", email], () =>
+  //   fetch(`http://localhost:5000/teams/${email}`, {
+  //     method: "GET",
+  //   }).then((res) => res.json())
+  // );
 
 
-  if (adminLoading || isLoading) {
-    return <p>loading....</p>
-  }
+ 
 
   return (
     <>
@@ -31,20 +36,7 @@ const InnerHome = () => {
         <input id="my-drawer" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content">
           <label htmlFor="my-drawer" className="btn ml-5 btn-primary drawer-button sm:mb-5 text-white">
-            {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 text-white"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M14 5l7 7m0 0l-7 7m7-7H3"
-              />
-            </svg> */}
+            
             My Teams
           </label>
           <Outlet></Outlet>
