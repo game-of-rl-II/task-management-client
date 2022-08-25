9553819ce4cbb067cf6a1a5174ccd6f671e6cd7b
faridulhaque <faridulhaquemurshed@gmail.com>
2022-08-25 19:29:00 +0600

    done

---
+++ src/Components/Dashboard/AdminDashboardItems/Settings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Settings = () => {
+    return (
+        <div>
+            <h2 className="mt-5 text-center text-3xl">We are working on it</h2>
+        </div>
+    );
+};
+
+export default Settings;
