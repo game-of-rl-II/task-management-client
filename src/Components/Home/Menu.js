f8eeba39324186ee5cecbda291d476ac449d0b3b
Arif6474 <arifulislam64743@gmail.com>
2022-08-29 03:45:59 +0600

    fix route bug

--- src/Components/Home/Menu.js
+++ src/Components/Home/Menu.js
@@ -17,15 +17,18 @@ const Menu = () => {
       <div class="navbar ">
         <div class="navbar-start">
           <div class="dropdown">
-            <label tabindex="0" class="btn btn-ghost lg:hidden">
+            <label tabindex="0" class="btn btn-teal-500 lg:hidden">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
             </label>
             <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2  shadow bg-teal-300 rounded-box w-52">
-              <li><Link className='underline ' to="addMember">Add Member</Link></li>
-
-              <li><Link className='underline ' to="manageEmployee">Manage Members</Link></li>
-              <li><Link className='underline' to="reviewYourTeamMember">Review a member</Link></li>
-            </ul>
+            <li><Link className='hover:bg-teal-700'to="addMember">Add Member</Link></li>
+            <li><Link className='hover:bg-teal-700' to="manageEmployee">Manage Members</Link></li>
+            <li><Link className='hover:bg-teal-700' to="reviewYourTeamMember">Review a member</Link></li>
+            <li><Link className='hover:bg-teal-700' to="employeeTask">Assigned tasks</Link></li>
+            <li><Link className='hover:bg-teal-700' to="todaysTask">Today's tasks</Link></li>
+            <li><Link className='hover:bg-teal-700' to="forwardedTask">Forwarded tasks</Link></li>
+            <li><Link className='hover:bg-teal-700' to="settings">Settings</Link></li>
+</ul>
           </div>
         </div>
         <div class="navbar-center hidden lg:flex">
