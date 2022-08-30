f510c2d68669f77e1cf072b8e1b58b4e53549d7f
al-amin <arifurr231@gmail.com>
2022-08-30 13:50:27 +0600

    task forward data load

--- src/Components/Tables/useAssignedTasksTable.js
+++ src/Components/Tables/useAssignedTasksTable.js
@@ -1,27 +1,32 @@
+const useAssignedTasksTable = ({ setMemberTaskModal }) => {
+  const assignedTasksColumns = [
+    {
+      name: <h2 className="pl-6 text-black text-lg font-bold">Member Name</h2>,
+      selector: (task) => <h2 className="pl-6 text-black text-md font-bold">{task.name}</h2>,
+    },
+    {
+      name: <h2 className="text-black text-lg font-bold">Member ID</h2>,
+      selector: (task) => <h2 className="text-black text-md font-bold">{task.memberId}</h2>,
+    },
+    {
+      name: <h2 className="text-black text-lg font-bold">Date</h2>,
+      selector: (task) => <h2 className={task.taskCompletion !== true && "text-teal-600 text-md"}>{task.taskDate}</h2>,
+    },
+    {
+      name: <h2 className="text-black text-lg font-bold">Details</h2>,
+      selector: (task) => (
+        <label
+          onClick={() => setMemberTaskModal(task)}
+          for="my-modal-4"
+          className="btn modal-button bg-teal-500 hover:bg-teal-700 text-white border-none btn-sm"
+        >
+          DETAILS
+        </label>
+      ),
+    },
+  ];
 
-const useAssignedTasksTable = ({setMemberTaskModal}) => {
-    const assignedTasksColumns =[
-        {
-            name: <h2  className="text-black text-lg font-bold">Member Name</h2>,
-            selector: task => <h2 className="text-black text-lg font-bold">{task.name}</h2>
-        },
-        {
-            name: <h2 className="text-black text-lg font-bold">Member ID</h2>,
-            selector: task => <h2 className="text-teal-500 text-lg font-bold">{task.memberId}</h2>
-        },
-        {
-            name: <h2 className="text-black text-lg font-bold">Date</h2>,
-            selector: task => <h2 className={task.taskCompletion !== true && 'text-teal-600'}>{task.taskDate}</h2>
-        },
-        {
-            name: <h2 className="text-black text-lg font-bold">Details</h2>,
-            selector: task => <label onClick={() => setMemberTaskModal(task)} for="my-modal-4" className="btn modal-button bg-teal-500 hover:bg-teal-700 text-white border-none btn-sm">
-            DETAILS
-          </label>
-        },
-    ]
-
-    return [assignedTasksColumns]
+  return [assignedTasksColumns];
 };
 
 export default useAssignedTasksTable;
--- src/Components/Tables/useForwardedTasksTable.js
+++ src/Components/Tables/useForwardedTasksTable.js
@@ -1,22 +1,26 @@
-const useForwardedTasksTable = () => {
-    const forwardedTasksTable = [
-        {
-            name: <h2>Forwarded By</h2>,
-            selector: task => <h2>{task?.name}</h2>
-        },
-        {
-            name: <h2>Assign Date</h2>,
-            selector: task => <h2>{task?.taskDate}</h2>
-        },
-        {
-            name: <h2>Deadline</h2>,
-            selector: task => <h2>{task?.deadline}</h2>
-        },
-        {
-            name: <h2>Message</h2>,
-            selector: task => <h2><button>view message</button></h2>
-        },
-    ]
-    return [forwardedTasksTable]
-}
+const useForwardedTasksTable = ({ setForwardTaskViewMessage }) => {
+  const forwardedTasksTable = [
+    {
+      name: <h2 className="pl-6 text-black text-lg font-bold">Forwarded By</h2>,
+      selector: (task) => <h2 className="pl-6 text-black text-md font-bold">{task?.name}</h2>,
+    },
+    {
+      name: <h2 className="text-black text-lg font-bold">Assign Date</h2>,
+      selector: (task) => <h2 className="text-black text-md font-bold">{task?.taskDate}</h2>,
+    },
+    {
+      name: <h2 className="text-black text-lg font-bold">Deadline</h2>,
+      selector: (task) => <h2 className="text-black text-md font-bold">{task?.deadline}</h2>,
+    },
+    {
+      name: <h2 className="text-black text-lg font-bold">Message</h2>,
+      selector: (task) => (
+        <label for="forward-task-view-message" onClick={() => setForwardTaskViewMessage(task)} class="btn btn-sm bg-teal-500 border-0">
+          view message
+        </label>
+      ),
+    },
+  ];
+  return [forwardedTasksTable];
+};
 export default useForwardedTasksTable;
